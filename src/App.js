2162ee67d2f9342a0c18bb8baa5bca64bc20b65a
tgalen <tgalen84@gmail.com>
2024-09-03 21:18:33 -0400

    add table head with mock categories

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Deal Log</div>;
+  const dealDataLabels = [
+    "Deal Number",
+    "Name",
+    "Rate Spread",
+    "Reserve",
+    "VSC",
+    "Gap",
+    "Tire & Wheel",
+    "Key",
+    "Appearance Protection",
+    "Advanced Protection",
+    "Advsnced Protection Plus",
+    "Windshield",
+  ];
+
+  return (
+    <div className="App">
+      Deal Log
+      <table border="1" cellPadding="10">
+        <thead>
+          <tr>
+            {dealDataLabels.map((label) => (
+              <th key={label}>{label}</th>
+            ))}
+          </tr>
+        </thead>
+      </table>
+    </div>
+  );
 }
 
 export default App;
