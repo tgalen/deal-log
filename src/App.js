0f27ae134f4dbf730a4838b72901b85e5bd7d43c
tgalen <tgalen84@gmail.com>
2024-09-04 10:49:44 -0400

    change dealDataLabels to an array of objects and render the value pair in table header

--- src/App.js
+++ src/App.js
@@ -2,19 +2,41 @@ import "./App.css";
 
 function App() {
   const dealDataLabels = [
-    "Deal Number",
-    "Business Manager",
-    "Customer Name",
-    "Rate Spread",
-    "Reserve",
-    "VSC",
-    "Gap",
-    "Tire & Wheel",
-    "Key",
-    "Appearance Protection",
-    "Advanced Protection",
-    "Advsnced Protection Plus",
-    "Windshield",
+    { dealNumber: "Deal Number" },
+    { deliveryDate: "Delivery Date" },
+    { businessManager: "Business Manager" },
+    { customerName: "Customer Name" },
+    { rateSpread: "Rate Spread" },
+    { reserve: "Reserve" },
+    { vsc: "VSC" },
+    { gap: "Gap" },
+    { tireWheel: "Tire & Wheel" },
+    { key: "Key" },
+    { appearenceProtection: "Appearance Protection" },
+    { advancedProtection: "Advanced Protection" },
+    { advancedProtectionPlus: "Advanced Protection Plus" },
+    { windshield: "Windshield" },
+  ];
+
+  const mockData = [
+    {
+      dealNumber: 989898,
+      deliveryDate: "9/1/2024",
+      businessManager: "Tim Galen",
+      customerName: "Bill Johnson",
+      rateSpread: 1.5,
+      reserve: 350,
+      vsc: 500,
+      gap: 300,
+    },
+    {
+      dealNumber: 444444,
+      deliveryDate: "9/2/2024",
+      businessManager: "Tim Galen",
+      customerName: "Betty Wilson",
+      rateSpread: 0,
+      reserve: 200,
+    },
   ];
 
   return (
@@ -24,7 +46,7 @@ function App() {
         <thead>
           <tr>
             {dealDataLabels.map((label) => (
-              <th key={label}>{label}</th>
+              <th key={label}>{Object.values(label)}</th>
             ))}
           </tr>
         </thead>
